a579627436ac2a195ec61d25ab42e38a57e5c04a
Muhammad Faiz <61232159+0Faiz0@users.noreply.github.com>
2021-08-20 01:44:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Form from './components/Form';
+import TodoList from './components/TodoList';
+import Loader from './components/Loader';
+//import {Container} from './components/TodoElement';
 
 function App() {
+
+  const [inputText,setInputText] = useState("");
+  const [Todos,setTodos] = useState([]);
+
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 4500)
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {
+      loading ?
+
+      <Loader/>
+
+      :
+
+      <div className="App">
+
+        <div className="head"><h1>TODO <span>APP</span></h1></div>
+        <Form Todos={Todos} setTodos={setTodos} inputText={inputText} setInputText={setInputText}/>
+        <TodoList setTodos={setTodos} Todos={Todos}/>
+      </div>
+    }
+    </>
   );
 }
 
